2ec4422679e41ced4df4ecb042f0062c71a4c1b1
BoardCompanions <karima.wood@boardcompanions.nl>
2026-06-25 16:31:31 +0100

    Add CORS handling and update API request structure

--- netlify/functions/monday.js
+++ netlify/functions/monday.js
@@ -1,20 +1,34 @@
 exports.handler = async function(event, context) {
   const API_KEY = "eyJhbGciOiJIUzI1NiJ9.eyJ0aWQiOjY3NDc2NTk3OCwiYWFpIjoxMSwidWlkIjoxMDQ2MTY2MzMsImlhZCI6IjIwMjYtMDYtMjRUMTA6NDg6MzkuNzcwWiIsInBlciI6Im1lOndyaXRlIiwiYWN0aWQiOjM1Mzk4NTM2LCJyZ24iOiJldWMxIn0.2PmOez-KyimI6xQIlCVo4xZugFR6nYr0_cpe_STl8B4";
 
+  // Handle CORS preflight
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type",
+        "Access-Control-Allow-Methods": "POST, OPTIONS"
+      },
+      body: ''
+    };
+  }
+
   try {
-    const body = JSON.parse(event.body);
+    const body = JSON.parse(event.body || '{}');
     
     const response = await fetch("https://api.monday.com/v2", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Authorization": API_KEY
+        "Authorization": API_KEY,
+        "API-Version": "2024-01"
       },
       body: JSON.stringify(body)
     });
 
-    const data = await response.json();
-
+    const text = await response.text();
+    
     return {
       statusCode: 200,
       headers: {
@@ -22,7 +36,7 @@ exports.handler = async function(event, context) {
         "Access-Control-Allow-Headers": "Content-Type",
         "Content-Type": "application/json"
       },
-      body: JSON.stringify(data)
+      body: text
     };
   } catch (error) {
     return {
